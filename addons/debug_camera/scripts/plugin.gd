68828c164ecb067503fdde696b2b217e03649f5b
Kcfresh53 <62062589+kcfresh53@users.noreply.github.com>
2024-05-13 09:02:16 -0400

    Update plugin.gd
    Fixed project history change on startup.

--- addons/debug_camera/scripts/plugin.gd
+++ addons/debug_camera/scripts/plugin.gd
@@ -3,8 +3,10 @@ extends EditorPlugin
 
 
 func _enter_tree():
-	add_autoload_singleton("DebugCam", "res://addons/debug_camera/scripts/DebugCamAutoload.gd")
+	if not ProjectSettings.has_setting("autoload/DebugCam"):
+		add_autoload_singleton("DebugCam", "res://addons/debug_camera/scripts/DebugCamAutoload.gd")
 
 
 func _exit_tree():
-	remove_autoload_singleton("DebugCam")
+	if ProjectSettings.has_setting("autoload/DebugCam"):
+		remove_autoload_singleton("DebugCam")
