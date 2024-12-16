9c7f9b15d498795f631125d4a25e57638067f970
Kcfresh53 <62062589+kcfresh53@users.noreply.github.com>
2024-12-15 21:17:22 -0500

    Update DebugCamAutoload.gd
    Fixes project crashes when transitioning between scenes

--- addons/debug_camera/scripts/DebugCamAutoload.gd
+++ addons/debug_camera/scripts/DebugCamAutoload.gd
@@ -2,15 +2,21 @@ extends Node
 
 const DEBUG_GUI = preload("res://addons/debug_camera/scenes/debug_gui.tscn")
 
-var cam_2d: DebugCamera2D = DebugCamera2D.new()
-var cam_3d: DebugCamera3D = DebugCamera3D.new()
-var gizmo_manager_3d: GizmoManager3D = GizmoManager3D.new()
-var edit_history_3d: EditHistory3D = EditHistory3D.new()
-var gui_instance := DEBUG_GUI.instantiate()
+var cam_2d: DebugCamera2D
+var cam_3d: DebugCamera3D
+var gizmo_manager_3d: GizmoManager3D
+var edit_history_3d: EditHistory3D
+var gui_instance: CanvasLayer
 
 
 func _ready() -> void:
 	get_tree().current_scene.tree_exited.connect(_new_scene)
+	
+	cam_2d = DebugCamera2D.new()
+	cam_3d = DebugCamera3D.new()
+	gizmo_manager_3d = GizmoManager3D.new()
+	edit_history_3d = EditHistory3D.new()
+	gui_instance = DEBUG_GUI.instantiate()
 	
 	if get_viewport().get_camera_2d() != null:
 		get_tree().current_scene.add_child(cam_2d)
